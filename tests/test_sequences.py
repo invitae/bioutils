1b463454fceaa37b83210de9180dd5b6ef09f521
Kevin Kazmierczak <kevin.kazmierczak@invitae.com>
2024-10-04 10:58:51 -0400

    feat: Adds new param for starts_at_first_codon that allows for placement of alternate initiator codons

--- tests/test_sequences.py
+++ tests/test_sequences.py
@@ -74,3 +74,17 @@ def test_translate_vertebrate_mitochondrial():
 )
 def test_translate_cds_w_exceptions(sequence, exception_map, translated_sequence):
     assert translate_cds(sequence, full_codons=False, ter_symbol="", exception_map=exception_map) == translated_sequence
+
+
+@pytest.mark.parametrize(
+    "sequence, translation_table, starts_at_first_codon, translated_sequence",
+    (
+        ("ATTAATCCC", TranslationTable.vertebrate_mitochondrial, True, "MNP"),
+        ("ATTATTAATCCC", TranslationTable.vertebrate_mitochondrial, True, "MINP"),
+        ("ATTAATCCC", TranslationTable.vertebrate_mitochondrial, False, "INP"),
+        ("ATTAATCCC", TranslationTable.standard, True, "INP"),
+        ("ATTAATCCC", TranslationTable.standard, False, "INP"),
+    ),
+)
+def test_translate_cds_mito_alts(sequence, translation_table, starts_at_first_codon, translated_sequence):
+    assert translate_cds(sequence, full_codons=False, ter_symbol="", translation_table=translation_table, starts_at_first_codon=starts_at_first_codon) == translated_sequence
